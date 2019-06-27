b5a479e26cb4928fd38859d740f87f82fa7925f6
zhangjiayi <zhangjiayi_0928@163.com>
2019-06-27 15:06:58 +0800

    feign

--- microservice-parent/microservice-consumer-feign/src/main/java/com/thunisoft/microserviceconsumerfeign/controller/UserConsumerController.java
+++ microservice-parent/microservice-consumer-feign/src/main/java/com/thunisoft/microserviceconsumerfeign/controller/UserConsumerController.java
@@ -2,6 +2,8 @@ package com.thunisoft.microserviceconsumerfeign.controller;
 
 import com.thunisoft.microservicecom.pojo.User;
 import com.thunisoft.microserviceconsumerfeign.service.ConsumerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserConsumerController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserConsumerController.class);
 
     @Autowired
     private ConsumerService consumerService;
 
     @RequestMapping(value = "/consumer/getUser")
     public User get() {
+        logger.info("do some work");
         return consumerService.getUser();
     }
 }
--- microservice-parent/microservice-provider3/src/main/java/com/thunisoft/microserviceprovider3/controller/MainController.java
+++ microservice-parent/microservice-provider3/src/main/java/com/thunisoft/microserviceprovider3/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.thunisoft.microserviceprovider3.controller;
 
 import com.thunisoft.microservicecom.pojo.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping(value = "/getUser", method = RequestMethod.GET)
     public User getUser() {
+        logger.info("do some work");
         User user = new User(3, "zjy", 21);
         return user;
     }
